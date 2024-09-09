3ee2f0d1542fb875b525fcf67c841fb94f71a8aa
Pollogamerz <85629754+Pollogamerz@users.noreply.github.com>
2024-09-09 10:42:15 -0600

    Playerinteraction

---
+++ Tribu/Assets/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public float interactionRange = 1.5f;
+    public LayerMask interactableLayer;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) || Input.GetMouseButtonDown(0))
+        {
+            CheckInteraction();
+        }
+    }
+
+    void CheckInteraction()
+    {
+        Collider2D[] interactables = Physics2D.OverlapCircleAll(transform.position, interactionRange, interactableLayer);
+        foreach (Collider2D interactable in interactables)
+        {
+            interactable.GetComponent<IInteractable>()?.Interact();
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, interactionRange);
+    }
+    public interface IInteractable
+    {
+        void Interact();¨ //éste apartado va a objetos interactuables
+    }
+}
