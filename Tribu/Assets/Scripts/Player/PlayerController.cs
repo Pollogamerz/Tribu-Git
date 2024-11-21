fa63e05bd388b452818ed23f43cc5f0db9d7b67f
Pollogamerz <85629754+Pollogamerz@users.noreply.github.com>
2024-11-20 22:51:49 -0600

    zona urbana terminada

--- Tribu/Assets/Scripts/Player/PlayerController.cs
+++ Tribu/Assets/Scripts/Player/PlayerController.cs
@@ -41,11 +41,11 @@ public class PlayerController : MonoBehaviour
 
         if (moveHorizontal < 0)
         {
-            transform.localScale = new Vector3(-4, 4, 4);
+            transform.localScale = new Vector3(-3, 3, 3);
         }
         if (moveHorizontal > 0)
         {
-            transform.localScale = new Vector3(4, 4, 4);
+            transform.localScale = new Vector3(3, 3, 3);
         }
     }
 
