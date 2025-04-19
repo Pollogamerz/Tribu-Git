63ba8ebb6ad66c094dc1a142cea48bf1850b0335
CVG42 <zap653@amerike.edu.mx>
2025-04-19 00:20:49 -0600

    AnimalData single script file

---
+++ Tribu/Assets/Scripts/Animals/AnimalData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class AnimalData
+{
+    public string animalName;
+    public AudioClip animalSound;
+    public Sprite animalSprite;
+    public string animalDescription;
+}
